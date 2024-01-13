bfac213926bb68b9341cb48da97a6243e214d18b
Rohit Bharti <rohitbharti279@gmail.com>
2024-01-13 23:21:05 +0530

    13. Roman to Integer final update

--- js/problem_13/main.js
+++ js/problem_13/main.js
@@ -7,7 +7,13 @@ var romanToInt = function(s) {
     let value = [1, 5, 10, 50, 100, 500, 1000];
     let result = 0;
     for (let i = 0; i < s.length; i++) {
-        result += value[symbol.indexOf(s[i])];
+        const currentValue = value[symbol.indexOf(s[i])];
+        const nextValue = value[symbol.indexOf(s[i + 1])];
+        if (nextValue > currentValue) {
+            result -= currentValue;
+        } else {
+            result += currentValue;
+        }
     }
 
     return result;
