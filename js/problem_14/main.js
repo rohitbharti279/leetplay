09315517e9766573245fe13531e1b58b8273474a
Rohit Bharti <rohitbharti279@gmail.com>
2024-01-14 15:22:56 +0530

    less time

--- js/problem_14/main.js
+++ js/problem_14/main.js
@@ -4,7 +4,8 @@
  */
 var longestCommonPrefix = function(strs) {
     let longestCommonPrefix = '';
-    for (let i = 0; i < strs[0].length; i++) {
+    let smallestString = strs.sort((a, b) => a.length - b.length);
+    for (let i = 0; i < smallestString[0].length; i++) {
         for (let j = 0; j < strs.length; j++) {
             if (strs[j][i]!== strs[0][i]) {
                 return longestCommonPrefix;
@@ -17,3 +18,4 @@ var longestCommonPrefix = function(strs) {
 
 console.log(longestCommonPrefix(["flower","flow","flight"]));
 console.log(longestCommonPrefix(["dog","racecar","car"]));
+console.log(longestCommonPrefix([""]));
