19ab45468ffae107f36531dc289dc89b46c0ec10
Rohit Bharti <rohitbharti279@gmail.com>
2024-01-16 14:37:06 +0530

    time exceeded error resolve

--- js/problem_15/main.js
+++ js/problem_15/main.js
@@ -1,24 +1,43 @@
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
-var threeSum = function(nums) {
-    let output = [];
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            for (let k = j + 1; k < nums.length; k++) {
-                if (nums[i] + nums[j] + nums[k] === 0) {
-                    const triplet = [nums[i], nums[j], nums[k]].sort((a, b) => a - b);
-                    if (!output.some(arr => arr.every((val, index) => val === triplet[index]))) {
-                        output.push(triplet);
-                    }
-                }
-            }
+function threeSum(nums) {
+    const result = [];
+  
+    nums.sort((a, b) => a - b);
+  
+    for (let i = 0; i < nums.length - 2; i++) {
+      if (i > 0 && nums[i] === nums[i - 1]) {
+        continue;
+      }
+  
+      let left = i + 1;
+      let right = nums.length - 1;
+  
+      while (left < right) {
+        const sum = nums[i] + nums[left] + nums[right];
+  
+        if (sum === 0) {
+          result.push([nums[i], nums[left], nums[right]]);
+  
+          while (left < right && nums[left] === nums[left + 1]) {
+            left++;
+          }
+  
+          while (left < right && nums[right] === nums[right - 1]) {
+            right--;
+          }
+  
+          left++;
+          right--;
+        } else if (sum < 0) {
+          left++;
+        } else {
+          right--;
         }
+      }
     }
-    return output;
-};
+  
+    return result;
+  }
 
 console.log(threeSum([-1, 0, 1, 2, -1, -4]));
-// console.log(threeSum([0,1,1]));
-// console.log(threeSum([0,0,0]));
+console.log(threeSum([0,1,1]));
+console.log(threeSum([0,0,0]));
